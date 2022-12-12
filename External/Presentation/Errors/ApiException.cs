9dfc2dd6c3f76a399e6dd1a03fe6e5d485c68d88
SubashAsusX515 <subash.barik@gmail.com>
2022-12-12 09:14:20 +0530

    Enhanced error msg for production.

--- External/Presentation/Errors/ApiException.cs
+++ External/Presentation/Errors/ApiException.cs
@@ -10,7 +10,7 @@ namespace Presentation.Errors
         public string Details { get; set; }
         public ApiException(int statusCode, string message = null,string details = null) : base(statusCode, message)
         {
-            Details = details ?? "An Error occurred while processing your request.";
+            Details = details ?? "An Error occurred while processing your request. Please contact your administrator.";
         }
     }
 }
