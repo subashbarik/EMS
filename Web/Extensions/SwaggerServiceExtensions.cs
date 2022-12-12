67b5b4c073a6e49878f910797a2b68dd79a5ed69
SubashAsusX515 <subash.barik@gmail.com>
2022-12-12 10:01:20 +0530

    Moved swagger configuration to its own file. which cleans up the program.cs file.

---
+++ Web/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Web.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services, Assembly presentationAssembly)
+        {
+            // API versioning configuration
+            services.AddApiVersioning(o =>
+            {
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                o.ReportApiVersions = true;
+                o.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("api-version"),
+                    new HeaderApiVersionReader("X-Version"),
+                    new MediaTypeApiVersionReader("ver"));
+            });
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+            // End API versioning 
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Description = "Contains API Infomation for Employee Management System version1",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Codeventurer Pvt. Ltd.",
+                        Email = "subash.barik@gmail.com",
+                        Url = new Uri("https://example.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under OpenApiLicense",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Version = "v2",
+                    Description = "Contains API Infomation for Employee Management System version2",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Codeventurer Pvt. Ltd.",
+                        Email = "barik.subash@gmail.com",
+                        Url = new Uri("https://example.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under OpenApiLicense",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                /*
+                    // Basic authentication
+                    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme  
+                            {  
+                                Name = "Authorization",  
+                                Type = SecuritySchemeType.Http,  
+                                Scheme = "basic",  
+                                In = ParameterLocation.Header,  
+                                Description = "Basic Authorization header using the Bearer scheme."  
+                            });  
+                */
+
+                // Sets up global security requirement - not need for now may be needed later
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type=ReferenceType.SecurityScheme,
+                //                Id="Bearer"
+                //            }
+                //        },new string[]{}
+                //    }
+                //});
+                /*
+                // Basic security requirement
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                            {  
+                                {  
+                                      new OpenApiSecurityScheme  
+                                        {  
+                                            Reference = new OpenApiReference  
+                                            {  
+                                                Type = ReferenceType.SecurityScheme,  
+                                                Id = "basic"  
+                                            }  
+                                        },  
+                                        new string[] {}  
+                                }  
+                            }); 
+                */
+                // Set the comments path for the Swagger JSON and UI.    
+                var xmlFile = $"{presentationAssembly.GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            return services;
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
 using Web;
 using Web.Extensions;
-using Web.Filters;
-using Web.Middleware;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,24 +16,7 @@ var presentationAssembly = typeof(Presentation.AssemblyReference).Assembly;
 builder.Services.AddControllers()
                 .AddApplicationPart(presentationAssembly);
 
-// API versioning configuration
-builder.Services.AddApiVersioning(o =>
-{
-    o.AssumeDefaultVersionWhenUnspecified = true;
-    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-    o.ReportApiVersions = true;
-    o.ApiVersionReader = ApiVersionReader.Combine(
-        new QueryStringApiVersionReader("api-version"),
-        new HeaderApiVersionReader("X-Version"),
-        new MediaTypeApiVersionReader("ver"));
-});
-builder.Services.AddVersionedApiExplorer(
-    options =>
-    {
-        options.GroupNameFormat = "'v'VVV";
-        options.SubstituteApiVersionInUrl = true;
-    });
-// End API versioning 
+
 //Add Appplication services such as DI and Others
 builder.Services.AddApplicationServices();
 //Add Database services
@@ -52,103 +31,8 @@ builder.Services.AddCors(opt =>
     });
 });
 //builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen( c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Description = "Contains API Infomation for Employee Management System version1",
-        TermsOfService =  new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Codeventurer Pvt. Ltd.",
-            Email = "subash.barik@gmail.com",
-            Url = new Uri("https://example.com"),
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Use under OpenApiLicense",
-            Url = new Uri("https://example.com/license"),
-        }
-    });
-    c.SwaggerDoc("v2", new OpenApiInfo
-    {
-        Version = "v2",
-        Description = "Contains API Infomation for Employee Management System version2",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Codeventurer Pvt. Ltd.",
-            Email = "barik.subash@gmail.com",
-            Url = new Uri("https://example.com"),
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Use under OpenApiLicense",
-            Url = new Uri("https://example.com/license"),
-        }
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "bearer"
-    });
-
-    /*
-        // Basic authentication
-        c.AddSecurityDefinition("basic", new OpenApiSecurityScheme  
-                {  
-                    Name = "Authorization",  
-                    Type = SecuritySchemeType.Http,  
-                    Scheme = "basic",  
-                    In = ParameterLocation.Header,  
-                    Description = "Basic Authorization header using the Bearer scheme."  
-                });  
-    */
-    
-    // Sets up global security requirement - not need for now may be needed later
-    //c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    //{
-    //    {
-    //        new OpenApiSecurityScheme
-    //        {
-
-    //            Reference = new OpenApiReference
-    //            {
-    //                Type=ReferenceType.SecurityScheme,
-    //                Id="Bearer"
-    //            }
-    //        },new string[]{}
-    //    }
-    //});
-    /*
-    // Basic security requirement
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement  
-                {  
-                    {  
-                          new OpenApiSecurityScheme  
-                            {  
-                                Reference = new OpenApiReference  
-                                {  
-                                    Type = ReferenceType.SecurityScheme,  
-                                    Id = "basic"  
-                                }  
-                            },  
-                            new string[] {}  
-                    }  
-                }); 
-    */
-    // Set the comments path for the Swagger JSON and UI.    
-    var xmlFile = $"{presentationAssembly.GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});  
+// Configures Swagger
+builder.Services.AddSwaggerServices(presentationAssembly);
 
 var app = builder.Build();
 
