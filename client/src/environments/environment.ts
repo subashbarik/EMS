72a0c6d109a851d515c8842502c2362312e9e339
SubashAsusX515 <subash.barik@gmail.com>
2022-12-13 12:37:39 +0530

    Points to the proper API for dev.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5293/api/',
-  apiUrl: 'http://localhost:5100/api/',
+  apiUrl: 'http://localhost:5293/api/',
+  //apiUrl: 'http://localhost:5100/api/',
   apiRetryCount: 3,
   apiRetryDelay: 3000,
 };
