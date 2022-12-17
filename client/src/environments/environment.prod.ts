97fb3c4e33f2a0a963edcc7a97b9d8682546fe9d
SubashAsusX515 <subash.barik@gmail.com>
2022-12-17 19:04:32 +0530

    change apiUrl to point to azure API

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5100/api/',
+  //apiUrl: 'http://localhost:5100/api/',
+  apiUrl: 'https://cvemsapi.azurewebsites.net/api/',
   apiRetryCount: 3,
   apiRetryDelay: 3000,
 };
